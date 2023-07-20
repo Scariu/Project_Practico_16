2fcdbf8921fecd8058c7aca914a5db7cedc6c6e7
Flopy <f.hidalgoalarcon@gmail.com>
2023-07-19 20:35:13 -0400

    Se implementa binding y método initAdapter en MainActivity.

--- app/src/main/java/com/example/project_practico_16/MainActivity.kt
+++ app/src/main/java/com/example/project_practico_16/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.project_practico_16
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.project_practico_16.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initAdapter()
+    }
+
+    private fun initAdapter() {
+        val adapter = Adapter()
+        val listaDePaises = PaisesLatam.paises
+        adapter.setData(listaDePaises)
+        binding.recyclerView.adapter = adapter
     }
 }

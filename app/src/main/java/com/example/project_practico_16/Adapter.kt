5a2581450078a4453c7ee9317f5fd9d6529106b9
Flopy <f.hidalgoalarcon@gmail.com>
2023-07-19 21:40:42 -0400

    Se agrega onClick a cardViewItem.

--- app/src/main/java/com/example/project_practico_16/Adapter.kt
+++ app/src/main/java/com/example/project_practico_16/Adapter.kt
@@ -1,7 +1,10 @@
 package com.example.project_practico_16
 
+import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import com.example.project_practico_16.databinding.ItemBinding
@@ -17,6 +20,7 @@ class Adapter : RecyclerView.Adapter<Adapter.ViewHolder>() {
     override fun onBindViewHolder(holder: Adapter.ViewHolder, position: Int) {
         val item = paises[position]
         holder.bind(item)
+
     }
 
     override fun getItemCount(): Int {
@@ -25,11 +29,17 @@ class Adapter : RecyclerView.Adapter<Adapter.ViewHolder>() {
     fun setData(listaPaises: List<Pais>){
         this.paises = listaPaises.toMutableList()
     }
-    class ViewHolder(val binding: ItemBinding): RecyclerView.ViewHolder(binding.root) {
+    class ViewHolder(val binding: ItemBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(itemPais: Pais) {
+            val texto = "${itemPais.nombre} tiene una poblacion de ${itemPais.poblacion} habitantes."
             binding.textViewPais.text = itemPais.nombre
             binding.imageViewPais.load(itemPais.imgUrl)
+            binding.cardViewItem.setOnClickListener(View.OnClickListener {
+                Toast.makeText(itemView.context, texto, Toast.LENGTH_LONG).show()
+            })
+
         }
 
     }
+
 }
